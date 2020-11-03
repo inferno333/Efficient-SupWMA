f15c73c00c5fa3778710f66900fdd5f8452b5489
inferno333 <inferno1968@proton.me>
2020-11-03 21:20:21 +0000

    json fix

--- SupWMA_TwoStage.sh
+++ SupWMA_TwoStage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # TODO: change to your Slicer and BRAINSFitCLI path
-Slicer=/home/victor/alarge/Softwares/S
+Slicer=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/Slicer
+BRAINSFitCLI=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-4.10/cli-modules/BRAIN
