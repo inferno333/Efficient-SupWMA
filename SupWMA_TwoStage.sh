2201d5ab730cfa9d93e4f483f5040dfb2645a750
inferno333 <inferno1968@proton.me>
2020-12-07 11:19:35 +0000

    change image size

--- SupWMA_TwoStage.sh
+++ SupWMA_TwoStage.sh
@@ -4,4 +4,6 @@
 Slicer=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/Slicer
 BRAINSFitCLI=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-4.10/cli-modules/BRAINSFit
 # export the defined environment variable which sets the path that the linker should look into while linking dynamic libraries/shared libraries.
-# TODO: You migh
+# TODO: You might not need to export paths
+export LD_LIBRARY_PATH=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-4.10/cli-modules/
+export LD_LIBRARY
