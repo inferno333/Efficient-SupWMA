c48ac7c68b196471d52a4db478c95ec28e582162
inferno333 <inferno1968@proton.me>
2020-12-07 08:42:40 +0000

    Create hello.py

--- SupWMA_TwoStage.sh
+++ SupWMA_TwoStage.sh
@@ -2,4 +2,6 @@
 
 # TODO: change to your Slicer and BRAINSFitCLI path
 Slicer=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/Slicer
-BRAINSFitCLI=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-4.10/cli-modules/BRAIN
+BRAINSFitCLI=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-4.10/cli-modules/BRAINSFit
+# export the defined environment variable which sets the path that the linker should look into while linking dynamic libraries/shared libraries.
+# TODO: You migh
