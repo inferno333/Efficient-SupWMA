19b8daa91eaefbfccd5c96476be29162c3e150ba
inferno333 <inferno1968@proton.me>
2020-12-07 21:23:26 +0000

    [DOCUMENTATION] Japanese sync

--- SupWMA_TwoStage.sh
+++ SupWMA_TwoStage.sh
@@ -6,4 +6,7 @@ BRAINSFitCLI=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-
 # export the defined environment variable which sets the path that the linker should look into while linking dynamic libraries/shared libraries.
 # TODO: You might not need to export paths
 export LD_LIBRARY_PATH=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-4.10/cli-modules/
-export LD_LIBRARY
+export LD_LIBRARY_PATH=/home/victor/alarge/Softwares/Slicer-4.10.2-linux-amd64/lib/Slicer-4.10/:$LD_LIBRARY_PATH
+
+# Trained model path
+model_folder=./TrainedModel_Tw
