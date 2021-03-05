c1e62b4372c65a0def40f7b18550f495dd1dd0a1
inferno333 <inferno1968@proton.me>
2021-03-05 13:53:30 +0000

    fg update

--- SupWMA_TwoStage.sh
+++ SupWMA_TwoStage.sh
@@ -29,4 +29,6 @@ wm_harden_transform.py ${subject_ukf} $output_folder $Slicer -t ${subject_transf
 # RAS feature extraction
 python ./extract_tract_feat.py ${output_folder}/${ukf_name} $output_folder -outPrefix ${subject_ID} -feature RAS -numPoints 15
 # SWM parcellation
-python ./test_TwoStage.py --weight_path ${model_folder} --feat_path $output_folder/${subject_ID}_featMatrix.h5 --out_path $output_folder --label_names ${mod
+python ./test_TwoStage.py --weight_path ${model_folder} --feat_path $output_folder/${subject_ID}_featMatrix.h5 --out_path $output_folder --label_names ${model_folder}/label_names.h5 --out_prefix ${subject_ID} --tractography_path ${subject_ukf}/${ukf_name}
+# Clean temp files
+rm -r ${output_folder}/${subject_ID}_feat
