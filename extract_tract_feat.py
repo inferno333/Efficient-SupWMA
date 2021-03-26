227b6bb4b021142d4cde52d981359fba23784f05
inferno333 <inferno1968@proton.me>
2021-03-26 21:28:05 +0000

    More README formatting

--- extract_tract_feat.py
+++ extract_tract_feat.py
@@ -14,4 +14,8 @@ def gen_features():
         feat_RAS = tract_feat.feat_RAS(pd_tract, number_of_points=args.numPoints)
 
         # Reshape from 3D (num of fibers, num of points, num of features) to 4D (num of fibers, num of points, num of features, 1)
-        # The 4D array considers the input has only one channel
+        # The 4D array considers the input has only one channel (depth = 1)
+        feat_shape = np.append(feat_RAS.shape, 1)
+        feat = np.reshape(feat_RAS, feat_shape)
+
+    elif args.feature == 'RAS-3D
