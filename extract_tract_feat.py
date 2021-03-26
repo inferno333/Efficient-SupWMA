146e62c8bc9acafbf1017d507dcd7542914da52c
inferno333 <inferno1968@proton.me>
2021-03-26 20:09:46 +0000

    - small fix

--- extract_tract_feat.py
+++ extract_tract_feat.py
@@ -13,4 +13,5 @@ def gen_features():
     if args.feature == 'RAS':
         feat_RAS = tract_feat.feat_RAS(pd_tract, number_of_points=args.numPoints)
 
-        # Reshape from 3D (num of fibers, num of point
+        # Reshape from 3D (num of fibers, num of points, num of features) to 4D (num of fibers, num of points, num of features, 1)
+        # The 4D array considers the input has only one channel
