5a2493f0f33b803f101632ea1d2e65637adebc4f
inferno333 <inferno1968@proton.me>
2021-04-26 20:34:01 +0000

    USB: option: fix port-data abuse

--- train_s2_classifier.py
+++ train_s2_classifier.py
@@ -73,3 +73,146 @@ def load_data():
 def train_val_net(supcon_net, classify_net):
     """train and validation of the network"""
     time_start = time.time()
+    train_num_batch = train_data_size / args.train_batch_size
+    val_num_batch = val_data_size / args.val_batch_size
+    # for save training and validating process data
+    train_loss_lst, val_loss_lst, train_acc_lst, val_acc_lst, \
+    train_precision_lst, val_precision_lst, train_recall_lst, val_recall_lst, \
+    train_f1_lst, val_f1_lst = [], [], [], [], [], [], [], [], [], []
+    # for best metrics
+    best_acc, best_f1_mac = 0, 0
+    best_acc_epoch, best_f1_epoch = 1, 1
+    best_acc_wts, best_f1_wts = None, None
+    best_acc_val_labels_lst, best_f1_val_labels_lst = [], []
+    best_acc_val_pred_lst, best_f1_val_pred_lst = [], []
+    for epoch in range(args.epoch):
+        train_start_time = time.time()
+        # epoch starts from 1
+        epoch += 1
+        total_train_loss, total_val_loss = 0, 0
+        train_labels_lst, train_predicted_lst = [], []
+        total_train_correct, total_val_correct = 0, 0
+        val_labels_lst, val_predicted_lst = [], []
+
+        # training
+        for i, data in enumerate(train_loader, 0):
+            points, label = data  # points [B, N, 3]
+            label = label[:, 0]  # [B,1] rank2 to (, B) rank1
+            points = points.transpose(2, 1)  # points [B, 3, N]
+            points, label = points.to(device), label.to(device)
+            optimizer.zero_grad()
+            supcon_net = supcon_net.eval()
+            classify_net = classify_net.train()
+            with torch.no_grad():
+                features = supcon_net.encoder(points)
+            pred = classify_net(features.detach())
+            loss = F.nll_loss(pred, label)
+            loss.backward()
+            optimizer.step()
+            if args.scheduler == 'wucd':
+                scheduler.step(epoch - 1 + i / train_num_batch)
+            _, pred_idx = torch.max(pred, dim=1)
+            correct = pred_idx.eq(label.data).cpu().sum()
+            # for calculating training accuracy and loss
+            total_train_correct += correct.item()
+            total_train_loss += loss.item()
+            # for calculating training weighted and macro metrics
+            label = label.cpu().detach().numpy().tolist()
+            train_labels_lst.extend(label)
+            pred_idx = pred_idx.cpu().detach().numpy().tolist()
+            train_predicted_lst.extend(pred_idx)
+        if args.scheduler == 'step':
+            scheduler.step()
+        # train accuracy loss
+        avg_train_acc = total_train_correct / float(train_data_size)
+        avg_train_loss = total_train_loss / float(train_num_batch)
+        train_acc_lst.append(avg_train_acc)
+        train_loss_lst.append(avg_train_loss)
+        # train macro p, r, f1
+        mac_train_precision, mac_train_recall, mac_train_f1 = calculate_prec_recall_f1(train_labels_lst,
+                                                                                       train_predicted_lst)
+        train_precision_lst.append(mac_train_precision)
+        train_recall_lst.append(mac_train_recall)
+        train_f1_lst.append(mac_train_f1)
+        train_end_time = time.time()
+        train_time = round(train_end_time - train_start_time, 2)
+        logger.info('{} epoch [{}/{}] time: {}s train loss: {} accuracy: {} f1: {}'.format(
+            script_name, epoch, args.epoch, train_time, round(avg_train_loss, 4), round(avg_train_acc, 4), round(mac_train_f1, 4)))
+
+        # validation
+        with torch.no_grad():
+            val_start_time = time.time()
+            for j, data in (enumerate(val_loader, 0)):
+                points, label = data
+                label = label[:, 0]
+                points = points.transpose(2, 1)
+                points, label = points.to(device), label.to(device)
+                supcon_net = supcon_net.eval()
+                classify_net = classify_net.eval()
+                features = supcon_net.encoder(points)
+                pred = classify_net(features)
+                loss = F.nll_loss(pred, label)
+                _, pred_idx = torch.max(pred, dim=1)
+                correct = pred_idx.eq(label.data).cpu().sum()
+                # for calculating validation accuracy and loss
+                total_val_correct += correct.item()
+                total_val_loss += loss.item()
+                # for calculating validation weighted and macro metrics
+                label = label.cpu().detach().numpy().tolist()
+                val_labels_lst.extend(label)
+                pred_idx = pred_idx.cpu().detach().numpy().tolist()
+                val_predicted_lst.extend(pred_idx)
+        # calculate the validation accuracy and loss for the epoch
+        avg_val_acc = total_val_correct / float(val_data_size)
+        avg_val_loss = total_val_loss / float(val_num_batch)
+        val_acc_lst.append(avg_val_acc)
+        val_loss_lst.append(avg_val_loss)
+        # calculate the validation macro metrics
+        mac_val_precision, mac_val_recall, mac_val_f1 = calculate_prec_recall_f1(val_labels_lst, val_predicted_lst)
+        val_precision_lst.append(mac_val_precision)
+        val_recall_lst.append(mac_val_recall)
+        val_f1_lst.append(mac_val_f1)
+        val_end_time = time.time()
+        val_time = round(val_end_time - val_start_time, 2)
+        logger.info('{} epoch [{}/{}] time: {}s val loss: {} accuracy: {} f1: {}'.format(
+            script_name, epoch, args.epoch, val_time, round(avg_val_loss, 4), round(avg_val_acc, 4), round(mac_val_f1, 4)))
+        # swap and save the best metric
+        if avg_val_acc > best_acc:
+            best_acc, best_acc_epoch, best_acc_wts, best_acc_val_labels_lst, best_acc_val_pred_lst = \
+                best_swap(avg_val_acc, epoch, classify_net, val_labels_lst, val_predicted_lst)
+        if mac_val_f1 > best_f1_mac:
+            best_f1_mac, best_f1_epoch, best_f1_wts, best_f1_val_labels_lst, best_f1_val_pred_lst = \
+                best_swap(mac_val_f1, epoch, classify_net, val_labels_lst, val_predicted_lst)
+
+    # save best weights
+    save_best_weights(classify_net, best_acc_wts, args.out_path, 'acc', best_acc_epoch, best_acc, logger)
+    save_best_weights(classify_net, best_f1_wts, args.out_path, 'f1', best_f1_epoch, best_f1_mac, logger)
+    # calculate classification report and plot class analysis curves for different metrics
+    label_names_str = [label_name.decode() for label_name in label_names]
+    # accuracy
+    classify_report(best_acc_val_labels_lst, best_acc_val_pred_lst, label_names_str, logger, args.out_path, 'acc')
+    per_class_metric(best_acc_val_labels_lst, best_acc_val_pred_lst, label_names_str, val_data_size, logger,
+                     args.out_path, 'acc')
+    # macro f1
+    classify_report(best_f1_val_labels_lst, best_f1_val_pred_lst, label_names_str, logger, args.out_path, 'f1')
+    per_class_metric(best_f1_val_labels_lst, best_f1_val_pred_lst, label_names_str, val_data_size, logger,
+                     args.out_path, 'f1')
+    if args.redistribute_class:
+        gen_199_classify_report(best_acc_val_labels_lst, best_acc_val_pred_lst, label_names_str, logger, args.out_path,
+                                'acc')
+        gen_199_classify_report(best_f1_val_labels_lst, best_f1_val_pred_lst, label_names_str, logger, args.out_path,
+                                'f1')
+
+    # plot process curves
+    process_curves(args.epoch, train_loss_lst, val_loss_lst, train_acc_lst, val_acc_lst,
+                   train_precision_lst, val_precision_lst, train_recall_lst, val_recall_lst,
+                   train_f1_lst, val_f1_lst, best_acc, best_acc_epoch, best_f1_mac, best_f1_epoch, args.out_path)
+    # total processing time
+    time_end = time.time()
+    total_time = round(time_end - time_start, 2)
+    logger.info('Total processing time is {}s'.format(total_time))
+    
+
+if __name__ == '__main__':
+    # Variable Space
+    parser = argparse.ArgumentParser(description="Train classification model in stage 2, and evaluate on validation data for entire 1 million streamlines",
