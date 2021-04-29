61667f65058e9ca51c6709449eb08d615d86697b
inferno333 <inferno1968@proton.me>
2021-04-29 15:05:52 +0000

    Update plan.md

--- utils/custom_loss.py
+++ utils/custom_loss.py
@@ -1,1 +1,7 @@
-# Reference from https://github.com/Ho
+# Reference from https://github.com/HobbitLong/SupContrast Yonglong Tian (yonglong@mit.edu)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class S
