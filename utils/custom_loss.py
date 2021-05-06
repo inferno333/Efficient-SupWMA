8edf35322f6fc30bbd3357c16301d252d3582411
inferno333 <inferno1968@proton.me>
2021-05-06 17:01:48 +0000

    clarifying comment

--- utils/custom_loss.py
+++ utils/custom_loss.py
@@ -6,4 +6,7 @@ import torch.nn.functional as F
 
 class SupConLoss(nn.Module):
     """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive lo
+    It also supports the unsupervised contrastive loss in SimCLR"""
+    def __init__(self, temperature=0.07, base_temperature=0.07):
+        super(SupConLoss, self).__init__()
+        self.temperature = te
