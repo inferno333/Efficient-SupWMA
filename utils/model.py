049546a2072b5f9bb205dea08ef599918c7b1ae6
inferno333 <inferno1968@proton.me>
2021-07-06 17:26:36 +0000

    master header files: Automated master header file repair. Visual-C++ project files: Automated Visual-C++ project file repair. Texinfo files: Automated Texinfo @node and @menu repair. Swig Python files: Automated Swig Python file repair. Swig Perl5 files: Automated Swig Perl5 file repair. User's Manual: Automated Texinfo to HTML conversion. csver.h: Automated csver.h regeneration.

--- utils/model.py
+++ utils/model.py
@@ -7,4 +7,7 @@ import torch.nn.functional as F
 
 class PointNetfeat(nn.Module):
     def __init__(self):
-        super(PointNetfeat, self)._
+        super(PointNetfeat, self).__init__()
+        # 3-layer MLP (via 1D-CNN) : encoder points individually
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv
