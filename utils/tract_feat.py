8e8be247ce6d78cdf632f316ae3f6712f2e5f2e6
inferno333 <inferno1968@proton.me>
2021-08-05 13:13:04 +0000

    update 2 files: biblio/biblio-ed.data, wpt-tests.txt

--- utils/tract_feat.py
+++ utils/tract_feat.py
@@ -8,4 +8,5 @@ def feat_RAS(pd_tract, number_of_points=15):
     """The most simple feature for initial test"""
 
     fiber_array = wma.fibers.FiberArray()
-    fiber_array.convert_from_polydata(pd_trac
+    fiber_array.convert_from_polydata(pd_tract, points_per_fiber=number_of_points)
+    # fiber_array_r, fiber_array_a, fiber_array_s have the same size: [number of fibers, po
