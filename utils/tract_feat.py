e013c8ef60ab16607f4a9f0bcf21e0ceb81662d3
inferno333 <inferno1968@proton.me>
2021-08-05 19:07:03 +0000

    modify bug

--- utils/tract_feat.py
+++ utils/tract_feat.py
@@ -9,4 +9,7 @@ def feat_RAS(pd_tract, number_of_points=15):
 
     fiber_array = wma.fibers.FiberArray()
     fiber_array.convert_from_polydata(pd_tract, points_per_fiber=number_of_points)
-    # fiber_array_r, fiber_array_a, fiber_array_s have the same size: [number of fibers, po
+    # fiber_array_r, fiber_array_a, fiber_array_s have the same size: [number of fibers, points of each fiber]
+    feat = np.dstack((fiber_array.fiber_array_r, fiber_array.fiber_array_a, fiber_array.fiber_array_s))
+
+    ret
