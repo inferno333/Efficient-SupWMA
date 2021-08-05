d2c4bb7157d91a4ca1fa4564f9fa2a4d1fd3eb3d
inferno333 <inferno1968@proton.me>
2021-08-05 10:34:54 +0000

    Create _posts

--- utils/tract_feat.py
+++ utils/tract_feat.py
@@ -4,4 +4,8 @@ import whitematteranalysis as wma
 import utils.fibers as fibers
 
 
-def feat_RAS(pd_tr
+def feat_RAS(pd_tract, number_of_points=15):
+    """The most simple feature for initial test"""
+
+    fiber_array = wma.fibers.FiberArray()
+    fiber_array.convert_from_polydata(pd_trac
