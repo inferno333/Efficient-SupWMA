a4569b3c3e25e8a3c609d68f93d1ca019ada623b
inferno333 <inferno1968@proton.me>
2021-08-19 16:07:23 +0000

    Updated wobble

--- utils/tract_feat.py
+++ utils/tract_feat.py
@@ -33,4 +33,9 @@ def feat_RAS_curv_tors(pd_tract, number_of_points=15):
     fiber_array.convert_from_polydata_with_trafic(pd_tract, points_per_fiber=number_of_points)
 
     feat = np.dstack((fiber_array.fiber_array_r, fiber_array.fiber_array_a, fiber_array.fiber_array_s,
-                      fiber_
+                      fiber_array.fiber_array_cur, fiber_array.fiber_array_tor))
+
+    return feat
+
+
+def feat_RAS_3D(pd_tract, number_of_points=15, repea
